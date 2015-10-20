139b6a1ba4b7865f230fe82baeb9c9c86779a3b3
NAKAGAKI Kenji <nakaken0629@gmail.com>
2015-10-20 22:05:51 +0900

    変数名の修正

--- CustomNavigationBar/CustomViewController.swift
+++ CustomNavigationBar/CustomViewController.swift
@@ -19,15 +19,15 @@ class CustomViewController : UIViewController {
             NSLayoutConstraint(item: titleView, attribute: .CenterX, relatedBy: .Equal, toItem: titleView.superview, attribute: .CenterX, multiplier: 1.0, constant: 0),
             NSLayoutConstraint(item: titleView, attribute: .CenterY, relatedBy: .Equal, toItem: titleView.superview, attribute: .CenterY, multiplier: 1.0, constant: 0)])
         
-        let titleText = UILabel()
-        titleText.translatesAutoresizingMaskIntoConstraints = false
-        titleText.text = "ここにタイトル"
-        titleText.sizeToFit()
-        titleView.addSubview(titleText)
-        print("titleText.frame = \(titleText.frame)")
-        titleText.superview?.addConstraints([
-            NSLayoutConstraint(item: titleText, attribute: .CenterX, relatedBy: .Equal, toItem: titleText.superview, attribute: .CenterX, multiplier: 1.0, constant: 0),
-            NSLayoutConstraint(item: titleText, attribute: .CenterY, relatedBy: .Equal, toItem: titleText.superview, attribute: .CenterY, multiplier: 1.0, constant: 0)])
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "ここにタイトル"
+        titleLabel.sizeToFit()
+        titleView.addSubview(titleLabel)
+        print("titleLabel.frame = \(titleLabel.frame)")
+        titleLabel.superview?.addConstraints([
+            NSLayoutConstraint(item: titleLabel, attribute: .CenterX, relatedBy: .Equal, toItem: titleLabel.superview, attribute: .CenterX, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: titleLabel, attribute: .CenterY, relatedBy: .Equal, toItem: titleLabel.superview, attribute: .CenterY, multiplier: 1.0, constant: 0)])
         
         let titleImage = UIImageView(image: UIImage(named: "SpeakerOn"))
         titleImage.translatesAutoresizingMaskIntoConstraints = false
@@ -35,7 +35,7 @@ class CustomViewController : UIViewController {
         titleView.addSubview(titleImage)
         print("titleImage.frame = \(titleImage.frame)")
         titleImage.superview?.addConstraints([
-            NSLayoutConstraint(item: titleImage, attribute: .Left, relatedBy: .Equal, toItem: titleText, attribute: .Right, multiplier: 1.0, constant: 8.0),
-            NSLayoutConstraint(item: titleImage, attribute: .CenterY, relatedBy: .Equal, toItem: titleText, attribute: .CenterY, multiplier: 1.0, constant: 0)])
+            NSLayoutConstraint(item: titleImage, attribute: .Left, relatedBy: .Equal, toItem: titleLabel, attribute: .Right, multiplier: 1.0, constant: 8.0),
+            NSLayoutConstraint(item: titleImage, attribute: .CenterY, relatedBy: .Equal, toItem: titleLabel, attribute: .CenterY, multiplier: 1.0, constant: 0)])
     }
 }
