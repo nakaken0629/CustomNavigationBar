112c3b14146c310d5569012ff672925b6aea3647
NAKAGAKI Kenji <nakaken0629@gmail.com>
2015-10-20 22:50:03 +0900

    UIButtonを使ってtitleとiconを設定する機能を追加

--- CustomNavigationBar/CustomViewController.swift
+++ CustomNavigationBar/CustomViewController.swift
@@ -12,30 +12,14 @@ class CustomViewController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let titleView = UIView()
-        titleView.translatesAutoresizingMaskIntoConstraints = false
-        self.navigationItem.titleView = titleView
-        titleView.superview?.addConstraints([
-            NSLayoutConstraint(item: titleView, attribute: .CenterX, relatedBy: .Equal, toItem: titleView.superview, attribute: .CenterX, multiplier: 1.0, constant: 0),
-            NSLayoutConstraint(item: titleView, attribute: .CenterY, relatedBy: .Equal, toItem: titleView.superview, attribute: .CenterY, multiplier: 1.0, constant: 0)])
-        
-        let titleLabel = UILabel()
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.text = "ここにタイトル"
-        titleLabel.sizeToFit()
-        titleView.addSubview(titleLabel)
-        print("titleLabel.frame = \(titleLabel.frame)")
-        titleLabel.superview?.addConstraints([
-            NSLayoutConstraint(item: titleLabel, attribute: .CenterX, relatedBy: .Equal, toItem: titleLabel.superview, attribute: .CenterX, multiplier: 1.0, constant: 0),
-            NSLayoutConstraint(item: titleLabel, attribute: .CenterY, relatedBy: .Equal, toItem: titleLabel.superview, attribute: .CenterY, multiplier: 1.0, constant: 0)])
-        
-        let titleImage = UIImageView(image: UIImage(named: "SpeakerOn"))
-        titleImage.translatesAutoresizingMaskIntoConstraints = false
-        titleImage.sizeToFit()
-        titleView.addSubview(titleImage)
-        print("titleImage.frame = \(titleImage.frame)")
-        titleImage.superview?.addConstraints([
-            NSLayoutConstraint(item: titleImage, attribute: .Left, relatedBy: .Equal, toItem: titleLabel, attribute: .Right, multiplier: 1.0, constant: 8.0),
-            NSLayoutConstraint(item: titleImage, attribute: .CenterY, relatedBy: .Equal, toItem: titleLabel, attribute: .CenterY, multiplier: 1.0, constant: 0)])
+        let titleButton = UIButton()
+        titleButton.userInteractionEnabled = false
+        titleButton.setTitle("２枚目。吾輩は猫である。名前はまだない。長いタイトルを入れたときにアイコンがどうなるか", forState: .Normal)
+        titleButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        titleButton.setImage(UIImage(named: "SpeakerOn"), forState: .Normal)
+        titleButton.frame = CGRectMake(0, 0, titleButton.frame.width + 44 + 8, 44)
+        titleButton.titleEdgeInsets = UIEdgeInsets(top: 0, left: -44, bottom: 0, right: 0)
+        titleButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 64, bottom: 0, right:-64)
+        self.navigationItem.titleView = titleButton
     }
 }
